a0ac6a939b08f7fd34dcf57b89f5a368c2f2aa3c
chaithanya krishna <32541042+chaitanyantr@users.noreply.github.com>
2025-03-06 15:37:52 -0800

    Update d360_serial_io.py
    readline error solved

--- digit360/ros2/d360/d360/d360_serial_io.py
+++ digit360/ros2/d360/d360/d360_serial_io.py
@@ -73,6 +73,27 @@ class TimeDelta:
         return None
 
 class Digit360():
+    class ReadLine:
+        def __init__(self, s):
+            self.buf = bytearray()
+            self.s = s
+        
+        def readline(self):
+            i = self.buf.find(b"\x00")
+            if i >= 0:
+                r = self.buf[:i+1]
+                self.buf = self.buf[i+1:]
+                return r
+            while True:
+                i = max(1, min(2048, self.s.in_waiting))
+                data = self.s.read(i)
+                i = data.find(b"\x00")
+                if i >= 0:
+                    r = self.buf + data[:i+1]
+                    self.buf[0:] = data[i+1:]
+                    return r
+                else:
+                    self.buf.extend(data)    
     def __init__(self, port: str, port_timeout: float=1.0) -> None:
         self._dev = serial.Serial(port, timeout=port_timeout)
         self.overruns = 0
